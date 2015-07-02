ac9cec87ea627023b90956f16b3dc764265f60f3
aalexeev239 <alexeev.andrey.a@gmail.com>
2015-07-03 02:45:45 +0300

    upd

--- src/js/modules/showPopup.js
+++ src/js/modules/showPopup.js
@@ -12,7 +12,13 @@ var showPopup = (function() {
     var $btn = $(this),
         $modal = $('[data-remodal-id="'+$btn.data('modal')+'"]');
 
-    if (is_touch_device() || !$modal.length) return;
+    if (is_touch_device() || !$modal.length) {
+      var target = $(this.hash);
+      $('html,body').animate({
+        scrollTop: target.offset().top - 120
+      }, 800);
+      return false;
+    };
 
     ev.preventDefault();
     $modal.remodal().open();
